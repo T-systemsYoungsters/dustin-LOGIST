87d1d91245fc835aeb66cbd77621f416be2b38b7
DPlewa <92516477+DPlewa@users.noreply.github.com>
2021-11-16 12:54:03 +0100

    LOGIST-207

---
+++ Tasks/15.11.21-19.11.21/python_chapter_1_worksheet.py
@@ -0,0 +1,56 @@
+# 3.
+print(2 / 3)
+print(2 // 3)
+
+# 4.
+pi = 3.14159265359
+print(pi)
+
+# 9.
+pi2 = float(3.14)
+print(pi2)
+
+# 10.
+radius = float(input("Radius:"))
+x = 3.14
+pi3 = x
+area = pi3  * radius ** 2
+print(area)
+
+# 11.
+x = 4
+y = 5
+a = ((x) * (y))
+print(a)
+
+# 12.
+x = 4
+y = 5
+a = 3(x + y)
+print(a)
+
+# 13.
+radius = input(float("Enter the radius:"))
+
+# 14.
+print(2/3+4)
+print(2 / 3 + 4)
+print(   2 /    3+    4  )
+
+# 18.
+print(" \" \n")
+
+# 19.
+print('Test 123')
+
+# 20.
+print(3 + 4 + 5 / 3)
+
+# 22.
+x = 3
+x + 1
+print(x)
+
+# 24.
+value = int(input("Enter your age"))
+print(value)
